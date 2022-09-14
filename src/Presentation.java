d2eae90b19f01e232189574a3ba61902581bd0b8
Touptwi <monsinjon.samuel@gmail.com>
2022-09-14 18:37:38 +0200

    Add JPanel gridPanel Add void setupGrid()

--- src/Presentation.java
+++ src/Presentation.java
@@ -5,6 +5,8 @@ public class Presentation extends JFrame {
 
     private Controller controller;
 
+    private JPanel gridPanel;
+
     public Presentation(Controller controller) {
         this.controller = controller;
         setupWindow();
@@ -14,10 +16,34 @@ public class Presentation extends JFrame {
         this.setTitle("Jewel Falls");
         this.setPreferredSize(new Dimension(1000, 800));
 
-        //TODO
+        setupGrid();
 
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    private void setupGrid() {
+        Grid grid = this.controller.getAbstraction().getGame().getGrid();
+        JPanel GridPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics gridPanelGraphics) {
+                super.paintComponent(gridPanelGraphics);
+                gridPanelGraphics.setColor(Color.BLACK);
+
+                for (int i=0; i<=grid.getRows(); i++) {
+                    int y = i*this.getHeight()/grid.getRows();
+                    gridPanelGraphics.drawLine(0,y,this.getWidth(),y);
+                }
+                for (int j=0; j<grid.getColumns(); j++) {
+                    int x = j*this.getWidth()/grid.getColumns();
+                    gridPanelGraphics.drawLine(x,0,x, this.getHeight());
+                }
+            }
+        };
+        
+        this.gridPanel = GridPanel;
+        this.add(this.gridPanel, BorderLayout.CENTER);
     }
 }
