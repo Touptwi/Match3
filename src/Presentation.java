f38ac0f93150dde9a35d96d6d8016998ddc56ba3
Touptwi <monsinjon.samuel@gmail.com>
2022-09-14 11:39:44 +0200

    Init Window

--- src/Presentation.java
+++ src/Presentation.java
@@ -1,5 +1,5 @@
-import javax.naming.ldap.Control;
 import javax.swing.*;
+import java.awt.*;
 
 public class Presentation extends JFrame {
 
@@ -7,6 +7,17 @@ public class Presentation extends JFrame {
 
     public Presentation(Controller controller) {
         this.controller = controller;
+        setupWindow();
+    }
 
+    private void setupWindow() {
+        this.setTitle("Jewel Falls");
+        this.setPreferredSize(new Dimension(1000, 800));
+
+        //TODO
+
+        this.pack();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
