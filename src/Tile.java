c101cd7b152d8616702b19ae183e655a6e96a0e2
Touptwi <monsinjon.samuel@gmail.com>
2022-09-21 12:32:49 +0200

    set Tile to abstract add Type add getters set the constructor

--- src/Tile.java
+++ src/Tile.java
@@ -1,6 +1,17 @@
-public class Tile {
+import java.awt.*;
 
-    public Tile() {
-        //TODO
+public abstract class Tile {
+
+    private Grid grid;
+
+    public enum Type {RED, GREEN, BLUE, PURPLE, YELLOW}
+    private Type type;
+
+    public Tile(Grid grid, Type type) {
+        this.grid = grid;
+        this.type = type;
     }
+
+    public Point getCoords() {return this.grid.getCoords(this);}
+    public Type getType() {return this.type;}
 }
