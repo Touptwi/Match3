f7b9f6c859b383aa5afd7e2e408098ac8f902cc5
Touptwi <monsinjon.samuel@gmail.com>
2022-10-05 00:09:39 +0200

    Setup switching Tiles

--- src/Grid.java
+++ src/Grid.java
@@ -5,6 +5,9 @@ public class Grid {
 
     private final int rows, columns;
     private ArrayList<ArrayList<Tile>> gridTable;
+    public enum Direction {NORTH, EAST, SOUTH, WEST}
+
+    public enum Type {RED_JEWEL, GREEN_JEWEL, BLUE_JEWEL, PURPLE_JEWEL, YELLOW_JEWEL}
 
     public Grid(int rows, int columns) {
         this.rows = rows;
@@ -55,4 +58,24 @@ public class Grid {
     public ArrayList<ArrayList<Tile>> getGridTable() {return this.gridTable;}
     public int getColumns() {return this.columns;}
     public int getRows() {return this.rows;}
+
+    private void switchTiles(Tile Tile1, Tile Tile2) {
+        if(Tile1!=null && Tile2!=null) {
+            Type temp = Tile1.getType();
+            Tile1.setType(Tile2.getType());
+            Tile2.setType(temp);
+        }
+    }
+
+    public void moveTileTo(Tile Tile, Direction direction) {
+        if(direction.equals(Direction.NORTH)){
+            switchTiles(Tile, getNeighbors(Tile).get(0));
+        } else if (direction.equals(Direction.EAST)) {
+            switchTiles(Tile, getNeighbors(Tile).get(1));
+        } else if (direction.equals(Direction.SOUTH)) {
+            switchTiles(Tile, getNeighbors(Tile).get(2));
+        } else if (direction.equals(Direction.WEST)) {
+            switchTiles(Tile, getNeighbors(Tile).get(3));
+        }
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -3,15 +3,18 @@ import java.awt.*;
 public abstract class Tile {
 
     private Grid grid;
+    private Grid.Type type;
 
-    public enum Type {RED_JEWEL, GREEN_JEWEL, BLUE_JEWEL, PURPLE_JEWEL, YELLOW_JEWEL}
-    private Type type;
-
-    public Tile(Grid grid, Type type) {
+    public Tile(Grid grid, Grid.Type type) {
         this.grid = grid;
         this.type = type;
     }
 
     public Point getCoords() {return this.grid.getCoords(this);}
-    public Type getType() {return this.type;}
+    public Grid.Type getType() {return this.type;}
+
+    public void setType(Grid.Type newType) {this.type = newType;}
+    public void moveTo(Grid.Direction Direction) {
+        this.grid.moveTileTo(this, Direction);
+    }
 }
