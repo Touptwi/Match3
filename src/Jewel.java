67e76e7a6024c3d1fff4e5d8d63700695f4cd4ca
Touptwi <monsinjon.samuel@gmail.com>
2022-10-05 00:28:39 +0200

    Setup switching Tiles

--- src/Jewel.java
+++ src/Jewel.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Jewel extends Tile {
 
     public Jewel(Grid grid) {
-        super(grid, Type.values()[new Random().nextInt(5)]);
+        super(grid, Grid.Type.values()[new Random().nextInt(5)]);
     }
 
 
